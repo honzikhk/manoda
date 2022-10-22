fbb5fc072ac833ede92371eae51d76fc0721fb3e
Honza <chudicekjan@gmail.com>
2022-10-22 07:23:36 +0200

    kw_in_period, number_of_values

--- energy_meter/views.py
+++ energy_meter/views.py
@@ -36,6 +36,7 @@ def chart_consumption():
     days_between_measuring = []
     values_lst = Record.objects.values_list("value", flat=True)[::-1]     # makes list of values
     dates_lst = Record.objects.values_list("date", flat=True)[::-1]  # makes list of dates from the oldest record
+    kw_in_period = []
 
     for i in range(len(dates_lst)):
         if i + 1 == len(dates_lst):
@@ -55,10 +56,16 @@ def chart_consumption():
         _ = values_lst[i + 1] - values_lst[i]       # diff between two measurements
         avg_consumption_in_period_per_day.append(round((_ / days_between_measuring[i]), 2))     # values for axe Y
 
+    for i in range(len(values_lst) - 1):
+        kw_in_period.append(values_lst[i + 1] - values_lst[i])
+    
+
     data["periods"] = periods
     data["avg_consumption_in_period_per_day"] = avg_consumption_in_period_per_day
     data["days_between_measuring"] = days_between_measuring
     data["values_lst"] = values_lst
+    data["number_of_values"] = len(values_lst)
+    data["kw_in_period"] = kw_in_period
 
     return data
     # return f"periods: {periods}, avg_consumption_in_period_per_day: {avg_consumption_in_period_per_day}," \
@@ -79,6 +86,7 @@ class RecordCreateView(CreateView):
             "number_of_days": count_avg_consumption()["count_of_days_int"],
             "avg_consumption": count_avg_consumption()["avg_consumption"],
             "price_per_month": count_avg_consumption()["price_per_month"],
+            
             
             "chart_consumption": chart_consumption(),
         })
