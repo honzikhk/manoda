2bf13d3d39a9de7a717f5020cce0ce01d6f042f2
Honza <chudicekjan@gmail.com>
2022-12-18 12:30:47 +0100

    updated requirements.txt

--- energy_meter/utils.py
+++ energy_meter/utils.py
@@ -15,7 +15,7 @@ def get_graph():
 
 def get_plot(x, y):
     plt.switch_backend("AGG")
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 5))     # size of chart
     plt.title("kws in periods")
     plt.bar(x, y)
     plt.xticks(rotation=90)
