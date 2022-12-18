0b329c8a84878dfd41dd9ebc0549995708630eaf
Honza <chudicekjan@gmail.com>
2022-12-18 12:45:33 +0100

    delete field >note< from model energymeter

--- energy_meter/models.py
+++ energy_meter/models.py
@@ -7,7 +7,6 @@ class Record(models.Model):     # maybe better name will be EnergyMeterRecord
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     value = models.IntegerField()
-    note = models.TextField(null=True, blank=True, max_length=500)
 
     def __str__(self):
         return f"Date: {self.date} - value: {self.value}"
